1277bd8628c55ed52eaca5400b58d365e700e24e
Luca Mannella <luca.mannella@polito.it>
2023-03-14 15:36:38 +0100

    adding asynch examples

---
+++ week03/async-examples/transcript.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const sqlite = require('sqlite3');
+
+const db = new sqlite.Database('transcript.sqlite',
+    (err) => { if (err) throw err; });
+
+let result = [];
+
+let sql = "SELECT * FROM course LEFT JOIN score ON course.code=score.coursecode" ;
+db.all(sql, (err,rows)=>{
+    if(err) throw err;
+    for (let row of rows) {
+        // console.log(row);  // --> Printing each row
+        result.push(row);     // --> storing the rows in an external array
+    }
+});
+
+console.log('*************');
+for (let row of result) {
+    console.log(row);        // --> trying to print the previously obtained rows
+}
