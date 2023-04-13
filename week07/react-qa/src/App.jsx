3673ab8f663341961eb17a93699c6add3cc76efa
Luigi De Russis <luigidr@gmail.com>
2023-04-13 11:32:06 +0200

    add code developed in class

--- week07/react-qa/src/App.jsx
+++ week07/react-qa/src/App.jsx
@@ -1,21 +1,41 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Question } from './QAModels';
+import { Question, Answer } from './QAModels';
 import { Container } from 'react-bootstrap';
 import Answers from './components/AnswerComponents';
 import NavHeader from './components/NavbarComponents';
 import QuestionDescription from './components/QuestionComponents';
+import { useState } from 'react';
 
 const fakeQuestion = new Question(1, 'Is JavaScript better than Python?', 'Luigi De Russis', '2023-02-07');
-fakeQuestion.init();
+const fakeAnswers = new Array(
+  new Answer(1, 'Yes', 'Luca Mannella', '2023-02-15', -10),
+  new Answer(2, 'Not in a million year', 'Guido van Rossum', '2023-03-02', 5),
+  new Answer(3, 'No', 'Luigi De Russis', '2023-03-02', 10),
+  new Answer(4, 'Both have their pros and cons', 'Mario Rossi', '2023-03-04'));
 
 function App() {
+  const [question, setQuestion] = useState(fakeQuestion);
+  const [answers, setAnswers] = useState(fakeAnswers);
+
+  const voteUp = (answerId) => {
+    setAnswers(oldAnswer => {
+      return oldAnswer.map((ans) => {
+        if(ans.id === answerId) {
+          // return the "updated" answer
+          return new Answer(ans.id, ans.text, ans.name, ans.date, ans.score +1);
+        }
+        else
+          return ans;
+      });
+    });
+  }
 
   return (
     <>
-      <NavHeader questionNum={fakeQuestion.id} />
+      <NavHeader questionNum={question.id} />
       <Container fluid className="mt-3">
-        <QuestionDescription question={fakeQuestion} />
-        <Answers answers={fakeQuestion.getAnswers()}></Answers>
+        <QuestionDescription question={question} />
+        <Answers answers={answers} voteUp={voteUp}></Answers>
       </Container>
     </>
     
--- week07/react-qa/src/components/AnswerComponents.jsx
+++ week07/react-qa/src/components/AnswerComponents.jsx
@@ -1,15 +1,17 @@
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { Row, Col, Table, Button } from 'react-bootstrap';
+import FormAnswer from './FormAnswer';
 
 function Answers(props) {
   return (
     <>
       <Row>
-        <Col as="h2">Answers:</Col>
+        <Col as="h2">Answers ({props.answers.length}):</Col>
       </Row>
       <Row>
         <Col lg={10} className="mx-auto">
-          <AnswerTable answers={props.answers}></AnswerTable>
+          <AnswerTable answers={props.answers} voteUp={props.voteUp}></AnswerTable>
+          <FormAnswer />
         </Col> 
       </Row>
     </>
@@ -30,7 +32,7 @@ function AnswerTable(props) {
       </thead>
       <tbody>
         {
-          props.answers.map((ans) => <AnswerRow answer={ans} key={ans.id} />)
+          props.answers.map((ans) => <AnswerRow answer={ans} key={ans.id} voteUp={props.voteUp}/>)
         }
       </tbody>
     </Table>
@@ -39,7 +41,7 @@ function AnswerTable(props) {
 
 function AnswerRow(props) {
   return(
-    <tr><AnswerData answer={props.answer}/><AnswerActions /></tr>
+    <tr><AnswerData answer={props.answer}/><AnswerActions voteUp={props.voteUp} answerId={props.answer.id}/></tr>
   );
 }
 
@@ -54,9 +56,9 @@ function AnswerData(props) {
   );
 }
 
-function AnswerActions() {
+function AnswerActions(props) {
   return <td>
-    <Button variant='success'><i className='bi bi-plus'></i></Button>
+    <Button variant='success' onClick={() => props.voteUp(props.answerId)}><i className='bi bi-plus'></i></Button>
     </td>
 }
 
