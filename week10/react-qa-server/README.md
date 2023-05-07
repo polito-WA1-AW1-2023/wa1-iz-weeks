5122035ef489dbcbe7b3fe3cc7eac8a5749f856e
Luigi De Russis <luigidr@gmail.com>
2023-05-07 16:47:37 +0200

    add next week's exercise and starting project

---
+++ week10/react-qa-server/README.md
@@ -0,0 +1,26 @@
+# `react-qa-server`
+
+The `react-qa-server` is the server-side app companion of HeapOverrun (i.e., `react-qa`). It presents some APIs to perform some CRUD operations on questions and their answers.
+
+## APIs
+Hereafter, we report the designed HTTP APIs, also implemented in the project.
+
+### __Title__
+
+URL: `/`
+
+HTTP Method: Pick one.
+
+Description: What I do.
+
+Request body:
+```
+A short example, if any
+```
+
+Response: How I reply.
+
+Response body:
+```
+A short example, if any
+```
---
+++ week10/react-qa-server/dao.js
@@ -0,0 +1,89 @@
+/* Data Access Object (DAO) module for accessing Q&A */
+/* Initial version taken from exercise 4 (week 03) */
+const sqlite = require('sqlite3');
+const {Question, Answer} = require('./QAModels');
+
+// open the database
+const db = new sqlite.Database('questions.sqlite', (err) => {
+  if (err) throw err;
+});
+
+/** QUESTIONS **/
+// get all the questions
+exports.listQuestions = () => {
+  return new Promise((resolve, reject) => {
+    const sql = 'SELECT * FROM questions';
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        reject(err);
+      }
+      const questions = rows.map((q) => new Question(q.id, q.text, q.author, q.date));
+      resolve(questions);
+    });
+  });
+}
+
+// get a question given its id
+exports.getQuestion = (id) => {
+  return new Promise((resolve, reject) => {
+    const sql = 'SELECT * FROM question WHERE id = ?';
+    db.get(sql, [id], (err, row) => {
+      if (err)
+        reject(err);
+      if (row == undefined)
+        resolve({error: 'Question not found.'}); 
+      else {
+        const question = new Question(row.id, row.text, row.author, row.date);
+        resolve(question);
+      }
+    });
+  });
+};
+
+// add a new question
+exports.addQuestion = (question) => {
+  return new Promise((resolve, reject) => {
+    const sql = 'INSERT INTO question(text, author, date) VALUES (?, ?, DATE(?))';
+    db.run(sql, [question.text, question.author, question.date], function(err) {
+      if(err) reject(err);
+      else resolve(this.lastID);
+    });
+  });
+};
+
+/** ANSWERS **/
+
+// get all the answer of a given question
+exports.listAnswersOf = (questionId) => {
+  return new Promise((resolve, reject) => {
+    const sql = 'SELECT * FROM answers WHERE questionId = ?';
+    db.all(sql, [questionId], (err, rows) => {
+      if (err) {
+        reject(err);
+      }
+      const answers = rows.map((a) => new Answer(a.id, a.text, a.name, a.date, a.questionId, a.score));
+      resolve(answers);
+    });
+  });
+};
+
+// add a new answer
+exports.addAnswer = (answer) => {
+  return new Promise ((resolve, reject) => {
+    const sql = 'INSERT INTO answers(text, author, date, score, questionId) VALUES (?, ?, DATE(?), ?, ?)';
+    db.run(sql, [answer.text, answer.author, answer.date, answer.score, answer.questionId], function(err) {
+      if(err) reject(err);
+      else resolve(this.lastID);
+    });
+  });
+};
+
+// update an existing answer
+exports.updateAnswer = (answer) => {
+  // write something clever
+}
+
+// vote for an answer
+exports.voteAnswer = (answerId, vote) => {
+  // write something clever
+}
